fe77b1b29d7ad9357ceb13121729c7f0e74eee18
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2025-08-27 22:18:10 +0700

    build(delete data folder): delete data folder

--- src/data/to-do-list.ts
+++
@@ -1,7 +0,0 @@
-import { IToDoItem } from "@/models/todo.item.model";
-
-export const toDoList: IToDoItem[] = [
-	{ id: 1, task: "Learn TypeScript", completed: false },
-	{ id: 2, task: "Build a ToDo App", completed: false },
-	{ id: 3, task: "Deploy to Vercel", completed: false },
-];
